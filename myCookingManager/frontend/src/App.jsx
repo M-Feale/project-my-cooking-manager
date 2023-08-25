75a09e27e9737aa82f9725e1db6d060def69ff89
M-Feale <marjolainefeale@gmail.com>
2023-08-25 16:39:08 -0400

    installed-react-router-dom-and-created-router-skeleton-in-App

--- myCookingManager/frontend/src/App.jsx
+++ myCookingManager/frontend/src/App.jsx
@@ -1,12 +1,18 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
 
-
   return (
-    <>
-      <h1>This is App!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Home Page!</h1>} />
+        <Route path="/signin" element={<h1>Welcome to Login!</h1>} />
+        <Route path="/catalog" element={<h1>Welcome the Cataloging Page !</h1>} />
+        <Route path="/recipes/:recipeId" element={<h1>Welcome to a specific recipe Page!</h1>} />
+        <Route path="*" element={<h1>Welcome to the ERROR page</h1>} />
+      </Routes>
+    </Router>
+      
   )
 }
 
