d740910487d97dd2729b3e581450ebc7c0d1852d
M-Feale <marjolainefeale@gmail.com>
2023-08-27 14:58:37 -0400

    imported-Home-component

--- myCookingManager/frontend/src/components/App.jsx
+++ myCookingManager/frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./Header";
+import Home from "./Home";
 
 const App = () => {
 
@@ -9,9 +10,9 @@ const App = () => {
       <GlobalStyles />
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Welcome to Home Page!</h1>} />
-        <Route path="/signin" element={<h1>Welcome to Login!</h1>} />
-        <Route path="/catalog" element={<h1>Welcome the Cataloging Page !</h1>} />
+        <Route path="/" element={ <Home />} />
+        <Route path="/recipes" element={<h1>Welcome to the Recipe Collection!</h1>} />
+        <Route path="/catalogue" element={<h1>Welcome the Cataloguing Page !</h1>} />
         <Route path="/recipes/:recipeId" element={<h1>Welcome to a specific recipe Page!</h1>} />
         <Route path="*" element={<h1>Welcome to the ERROR page</h1>} />
       </Routes>
