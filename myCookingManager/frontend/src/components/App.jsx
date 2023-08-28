f4ca8ca0b44c1d5ae13c233b1ee8cedbf292345f
M-Feale <marjolainefeale@gmail.com>
2023-08-27 21:07:47 -0400

    imported-RecipeCollection-to-App

--- myCookingManager/frontend/src/components/App.jsx
+++ myCookingManager/frontend/src/components/App.jsx
@@ -1,24 +1,29 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./Header";
 import Home from "./Home";
+import RecipeCollection from "./RecipeCollection";
 
 const App = () => {
+	return (
+		<Router>
+			<GlobalStyles />
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/recipes" element={<RecipeCollection />} />
+				<Route
+					path="/catalogue"
+					element={<h1>Welcome the Cataloguing Page !</h1>}
+				/>
+				<Route
+					path="/recipes/:recipeId"
+					element={<h1>Welcome to a specific recipe Page!</h1>}
+				/>
+				<Route path="*" element={<h1>Welcome to the ERROR page</h1>} />
+			</Routes>
+		</Router>
+	);
+};
 
-  return (
-    <Router>
-      <GlobalStyles />
-      <Header />
-      <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/recipes" element={<h1>Welcome to the Recipe Collection!</h1>} />
-        <Route path="/catalogue" element={<h1>Welcome the Cataloguing Page !</h1>} />
-        <Route path="/recipes/:recipeId" element={<h1>Welcome to a specific recipe Page!</h1>} />
-        <Route path="*" element={<h1>Welcome to the ERROR page</h1>} />
-      </Routes>
-    </Router>
-      
-  )
-}
-
-export default App
+export default App;
