2273f47e9b82dcddc20181347e7d8a129d846ec2
M-Feale <marjolainefeale@gmail.com>
2023-08-28 18:02:55 -0400

    created-constants-file-for-DB-constant-variables

---
+++ myCookingManager/backend/constants.js
@@ -0,0 +1,16 @@
+// Constants used for the BE
+
+// Database name
+const DB_NAME = "my_cooking_manager";
+
+// User collection name
+const US_COLL = "users";
+
+// Recipe collection name
+const RE_COLL = "recipes";
+
+module.exports = {
+    DB_NAME,
+    US_COLL,
+    RE_COLL
+}
