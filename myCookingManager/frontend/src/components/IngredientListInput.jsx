ce28be4c0ea934d4cedc07a47f8d2c68874f1e59
M-Feale <marjolainefeale@gmail.com>
2023-08-30 11:10:15 -0400

    formatted-textarea-and-cleaned-up-code

--- myCookingManager/frontend/src/components/IngredientListInput.jsx
+++ myCookingManager/frontend/src/components/IngredientListInput.jsx
@@ -1,29 +1,12 @@
 const IngredientListInput = () => {
 	const handleSubmit = (e) => {
-		// Prevent the browser from reloading the page
 		e.preventDefault();
 
 		// Read the form data
 		const form = e.target;
 		const formData = new FormData(form);
-
-		// console.log(formData, "this is formdata");
-
-		// const requestBody = {
-		// 	ingredientList: '1 cup potato, chopped\n1 cup parsley, minced\n230 grams cheese',
-		// 	servings: 1,
-		// 	includeNutrition: false
-		// }
-
-		// const searchParams = new URLSearchParams(requestBody)
-		// console.log(searchParams.toString(), "this is search params")
-
-		// Or you can work with it as a plain object:
-		const formJson = Object.fromEntries(formData.entries());
-		console.log(formJson, "this is formJson");
-		// const searchParams = new URLSearchParams(formData)
-		// console.log(searchParams, "might be uri encoded like I want ?")
-		// console.log(searchParams.toString(), "is this it?")
+		// Convert it to an object with ingredientList as a key and the value of the textarea as a value
+		const ingredientObject = Object.fromEntries(formData.entries());
 
 		fetch("/api/ingredient-list", {
 			method: "POST",
@@ -31,29 +14,33 @@ const IngredientListInput = () => {
 				Accept: "application/json",
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(formJson),
+			body: JSON.stringify(ingredientObject),
 		})
 			.then((response) => response.json())
 			.then((parsedResponse) => {
-				console.log(parsedResponse, "this is the response from the textinput fetch");
+				console.log(
+					parsedResponse,
+					"this is the response from the textinput fetch"
+				);
 			});
 	};
 
 	return (
-		<form method="post" onSubmit={handleSubmit}>
-			<label>
-				Edit your post:
-				<textarea
-					name="ingredientList"
-					defaultValue=""
-					rows={4}
-					cols={40}
-				/>
-			</label>
-			<hr />
-			<button type="reset">Reset edits</button>
-			<button type="submit">Save post</button>
-		</form>
+		<>
+			<form method="post" onSubmit={handleSubmit}>
+				<label>
+					Create your ingredient list
+					<textarea
+						name="ingredientList"
+						placeholder="Paste your ingredient list with a carriage return (Pressing 'Enter') between each ingredient"
+						rows={12}
+						cols={80}
+					/>
+				</label>
+				<button type="reset">Reset</button>
+				<button type="submit">Save List</button>
+			</form>
+		</>
 	);
 };
 
