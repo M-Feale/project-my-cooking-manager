a880e2ec41a567cd33284cc537bf8a82de04124b
M-Feale <marjolainefeale@gmail.com>
2023-08-29 23:27:11 -0400

    WIP-updated-ingredient-parsing-handler

--- myCookingManager/backend/extAPI_handlers.js
+++ myCookingManager/backend/extAPI_handlers.js
@@ -3,32 +3,35 @@ require("dotenv").config();
 
 const fetch = require("node-fetch")
 
+const createShoppingList = async (req, res) => {
+
+    const { ingredientList } = req.body;
+    console.log(ingredientList, "this is ingredient list")
 
     const requestBody = {
-        ingredientList: '1 cup potato, chopped\n1 tsp parsley, minced\n230 grams cheese, grated',
+        ingredientList: ingredientList,
         servings: 1,
         includeNutrition: false
     }
 
     const searchParams = new URLSearchParams(requestBody)
-
-    // console.log(searchParams.toString(), "this is url searParams")
     const parsingQuery = searchParams.toString();
     console.log(parsingQuery, "parsing query")
 
-const createShoppingList = async (req, res) => {
-
     try {
-        // const response = await fetch(`https://api.spoonacular.com/recipes/parseIngredients?apiKey=${SPOON_KEY}`, {
-        //     method: "POST",
-        //     header: {
-        //         "Content-Type": "application/x-www-form-urlencoded"
-        //     },
-        //     body: parsingQuery
-        // })
-        // console.log(response.headers, "these are the headers")
-        // const data = await response.json()
-        // console.log(data, "this is parsed body")
+        const response = await fetch(`https://api.spoonacular.com/recipes/parseIngredients?apiKey=${SPOON_KEY}`, {
+            method: "POST",
+            header: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: parsingQuery
+        })
+        console.log(response.headers, "these are the headers")
+        const data = await response.json()
+        console.log(data, "this is parsed body")
+        if (data.length > 0) {
+            return res.status(200).json({status: 200, message: "Succes", data: data})
+        }
     }
 
     catch (err) {
