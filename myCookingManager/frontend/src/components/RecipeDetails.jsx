d33705026aa049909cf2e6fa3f7c6bb0049496bd
M-Feale <marjolainefeale@gmail.com>
2023-08-31 07:30:13 -0400

    added-styling-and-conditional-rendering-to-the-page

--- myCookingManager/frontend/src/components/RecipeDetails.jsx
+++ myCookingManager/frontend/src/components/RecipeDetails.jsx
@@ -1,6 +1,10 @@
 import { useContext, useEffect } from "react";
-import IngredientListInput from "./IngredientListInput";
+
 import { RecipeDetailsContext } from "./RecipeDetailsContext";
+
+import DetailsPreview from "./DetailsPreview";
+import IngredientListInput from "./IngredientListInput";
+import { styled } from "styled-components";
 
 const RecipeDetails = () => {
 	// I'll need to see how I can access the UserContext from auth0.
@@ -13,19 +17,21 @@ const RecipeDetails = () => {
 	const { currentRecipeDetails, setCurrentRecipeDetails } =
 		useContext(RecipeDetailsContext);
 
-	// On mount, fetch recipe associated with the specific recipe's page we are on. 
+	// On mount, fetch recipe associated with the specific recipe's page we are on.
 	// Store the data in the RecipeDetailsContext for the other components to have easy access to it.
 	useEffect(() => {
-
 		// If the context is empty or if the context contains a recipe different from the current RecipeDetails' page we are on, fetch the details for the page we're on.
-		if (!currentRecipeDetails.recipeId && currentRecipeDetails.recipeId !== recipeId) {
+		if (
+			!currentRecipeDetails.recipeId &&
+			currentRecipeDetails.recipeId !== recipeId
+		) {
 			fetch(`/api/user/${userId}/recipes/${recipeId}`)
 				.then((res) => res.json())
 				.then((parsedResponse) => {
-					if (parsedResponse.status === 200){
-						setCurrentRecipeDetails(parsedResponse.data)
+					if (parsedResponse.status === 200) {
+						setCurrentRecipeDetails(parsedResponse.data);
 					} else {
-						throw new Error(parsedResponse)
+						throw new Error(parsedResponse);
 					}
 				})
 				.catch((error) => {
@@ -36,9 +42,26 @@ const RecipeDetails = () => {
 
 	return (
 		<>
-			<IngredientListInput />
+			{currentRecipeDetails?.recipeId !== recipeId ? (
+				<h1>Loading...</h1>
+			) : (
+				<Wrapper>
+					<>
+						<DetailsPreview />
+					</>
+					<>
+						<IngredientListInput />
+					</>
+				</Wrapper>
+			)}
 		</>
 	);
 };
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+`;
 
 export default RecipeDetails;
