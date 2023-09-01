da4afd01b2eb6bcc933f35a5fd3a8bfe1de3b9a8
M-Feale <marjolainefeale@gmail.com>
2023-09-01 13:05:31 -0400

    added-conditional-rendering-but-still-needs-work

--- myCookingManager/frontend/src/components/RecipePreview.jsx
+++ myCookingManager/frontend/src/components/RecipePreview.jsx
@@ -11,54 +11,61 @@ const RecipePreview = () => {
 
 	return (
 		<Wrapper>
-			<TextAndDialogueBoxContainer>
-				<TextContainer>
-					<RecipeName>
-						<SectionTitle>Recipe name</SectionTitle>
-						{catalogueFlow.recipeInfo.name}
-					</RecipeName>
-					<WebsiteName>
-						<SectionTitle>Website name</SectionTitle>
-						{catalogueFlow.recipeInfo.website}
-					</WebsiteName>
-					<RecipeDescription>
-						<SectionTitle>Recipe description</SectionTitle>
-						{catalogueFlow.recipeInfo.description}
-					</RecipeDescription>
-				</TextContainer>
-				<DialogueBoxContainer>
-					<DialogueBox
-						title={
-							"Is this the recipe you were looking to add to your collection ?"
-						}
-						buttonArray={[
-							{
-								text: "Yes",
-								function: () =>
-									setCatalogueFlow({
-										...catalogueFlow,
-										isRecipePreviewCorrect: true,
-									}),
-							},
-							{
-								// I want to make the input field in focus when "No" is clicked. As it is now, it just makes the whole RecipePreview disappear and it's pretty jarring.
-								text: "No",
-								function: () =>
-									setCatalogueFlow({
-										...catalogueFlow,
-										isRecipeInput: false,
-									}),
-							},
-						]}
-					/>
-				</DialogueBoxContainer>
-			</TextAndDialogueBoxContainer>
-			<ImagePreviewContainer>
-				<SectionTitle>Preview Image</SectionTitle>
-				<ImageDiv>
-					<Image src={catalogueFlow.recipeInfo.image} />
-				</ImageDiv>
-			</ImagePreviewContainer>
+			{!catalogueFlow.recipeInfo.image ? (
+				<h1>Loading...</h1>
+			) : (
+				<>
+					<TextAndDialogueBoxContainer>
+						<TextContainer>
+							<RecipeName>
+								<SectionTitle>Recipe name</SectionTitle>
+								{catalogueFlow.recipeInfo.name}
+							</RecipeName>
+							<WebsiteName>
+								<SectionTitle>Website name</SectionTitle>
+								{catalogueFlow.recipeInfo.website}
+							</WebsiteName>
+							<RecipeDescription>
+								<SectionTitle>Recipe description</SectionTitle>
+								{catalogueFlow.recipeInfo.description}
+							</RecipeDescription>
+						</TextContainer>
+						<DialogueBoxContainer>
+							<DialogueBox
+								title={
+									"Is this the recipe you were looking to add to your collection ?"
+								}
+								buttonArray={[
+									{
+										text: "Yes",
+										function: () =>
+											setCatalogueFlow({
+												...catalogueFlow,
+												isRecipePreviewCorrect: true,
+											}),
+									},
+									{
+										// I want to make the input field in focus when "No" is clicked. As it is now, it just makes the whole RecipePreview disappear and it's pretty jarring.
+										text: "No",
+										function: () =>
+											setCatalogueFlow({
+												...catalogueFlow,
+												isRecipePreviewCorrect: false,
+											}),
+									},
+								]}
+							/>
+						</DialogueBoxContainer>
+					</TextAndDialogueBoxContainer>
+					<ImagePreviewContainer>
+						<SectionTitle>Preview Image</SectionTitle>
+						{/* $url={catalogueFlow.recipeInfo.image} */}
+						<ImageDiv>
+							<Image src={catalogueFlow.recipeInfo.image} />
+						</ImageDiv>
+					</ImagePreviewContainer>
+				</>
+			)}
 		</Wrapper>
 	);
 };
@@ -81,8 +88,8 @@ const RecipeName = styled.p`
 const SectionTitle = styled.span`
 	display: block;
 	font-weight: bold;
-    font-size: 20px;
-    margin: 10px 0;
+	font-size: 20px;
+	margin: 10px 0;
 `;
 
 const WebsiteName = styled(RecipeName)``;
@@ -98,20 +105,27 @@ const TextContainer = styled.div`
 
 const DialogueBoxContainer = styled.div`
 	flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
-    align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: end;
+	align-items: center;
 `;
 
 const ImagePreviewContainer = styled.div`
 	flex-grow: 1;
 `;
 
-const ImageDiv = styled.div``;
+const ImageDiv = styled.div`
+	height: 450px;
+	overflow: hidden;
+	/* background-image: url(${(props) => props.$url}); */
+`;
 
 const Image = styled.img`
-	width: 300px;
+	/* max-width: 100%; */
+	height: 100%;
+	display: block;
+	/* margin: 0 auto; */
 `;
 
 export default RecipePreview;
