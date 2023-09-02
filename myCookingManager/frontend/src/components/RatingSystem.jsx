26837118db074d56000b69615c046bd159b4e7b5
M-Feale <marjolainefeale@gmail.com>
2023-09-02 16:11:30 -0400

    wired-Rating-System-to-BE

--- myCookingManager/frontend/src/components/RatingSystem.jsx
+++ myCookingManager/frontend/src/components/RatingSystem.jsx
@@ -1,15 +1,49 @@
 import { Rating } from "@smastrom/react-rating";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import "@smastrom/react-rating/style.css";
 import { styled } from "styled-components";
 import { RecipeDetailsContext } from "./RecipeDetailsContext";
 
 const RatingSystem = () => {
+	// temporary user id
+	const userId = 1234;
+
 	const { currentRecipeDetails, setCurrentRecipeDetails } =
 		useContext(RecipeDetailsContext);
 
+	const [wereRatingsEdited, setWereRatingsEdited] = useState(false);
+
+	useEffect(() => {
+		if (wereRatingsEdited) {
+			console.log("the ratings have changed!!!");
+			fetch(`/api/user/${userId}/recipes/${currentRecipeDetails.recipeId}/update`, {
+				method: "PATCH",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					info: { ratings: currentRecipeDetails.ratings },
+				}),
+			})
+				.then((response) => response.json())
+				.then((parsedResponse) => {
+					if (parsedResponse.status === 200) {
+						// Decide if I want to add a success message for a successful ingredient list update
+						console.log(parsedResponse);
+					} else {
+						throw new Error(parsedResponse.message);
+					}
+				})
+				.catch((error) => {
+					console.error("Fetch error:", error);
+				});
+		}
+	}, [currentRecipeDetails.ratings]);
+
 	const handleRating = (rating, label) => {
+		setWereRatingsEdited(true);
 		const currentRatings = [...currentRecipeDetails.ratings];
 		const foundRating = currentRatings.find((rating) => {
 			return rating.label === label;
