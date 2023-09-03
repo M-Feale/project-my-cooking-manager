f4933b66dbe33ead63f89ad52aeb2ec8eb1130f6
M-Feale <marjolainefeale@gmail.com>
2023-09-03 11:20:17 -0400

    completed-grid-styling-and-RecipeCard-component

--- myCookingManager/frontend/src/components/RecipeCard.jsx
+++ myCookingManager/frontend/src/components/RecipeCard.jsx
@@ -1,8 +1,10 @@
 import { Rating } from "@smastrom/react-rating";
 import { useNavigate } from "react-router";
 import { styled } from "styled-components";
+
 import { lastDateCalculator } from "../../utility_functions/lastDateCalculator";
-import { FaListUl, FaStickyNote } from "react-icons/fa";
+import { FaListUl } from "react-icons/fa";
+import { FaRegNoteSticky } from "react-icons/fa6";
 
 const RecipeCard = ({ recipe }) => {
 	const navigate = useNavigate();
@@ -22,34 +24,36 @@ const RecipeCard = ({ recipe }) => {
 			<ImageContainer>
 				<Image src={recipe.image} alt={recipe.name} />
 			</ImageContainer>
-
-			<TextContainer>
-				<h1>{recipe.name}</h1>
-				<p>by {recipe.website}</p>
-				<p>{recipe.category}</p>
-				<Rating
-					style={{ maxWidth: 100 }}
-					readOnly={true}
-					value={overallRating.rating}
-				/>
-				<p>Last made: {lastDateMade}</p>
-				<ListIcon />
-				<FaStickyNote />
-			</TextContainer>
+			<RecipeName>
+				{recipe.name.length > 35
+					? `${recipe.name.substring(0, 35)}...`
+					: recipe.name}
+			</RecipeName>
+			<TextAndIconContainer>
+				<LeftContainer>
+					<p>by {recipe.website}</p>
+					<p>Category: {recipe.category}</p>
+					<Rating
+						style={{ maxWidth: 150 }}
+						readOnly={true}
+						value={overallRating.rating}
+					/>
+					<p>
+						Last made:{" "}
+						{recipe.dates_created.length ? lastDateMade : "Never"}
+					</p>
+				</LeftContainer>
+				<RightContainer>
+					<ListIcon $isPresent={recipe.shopping_list.length} />
+					<NoteIcon $isPresent={recipe.notes.length} />
+				</RightContainer>
+			</TextAndIconContainer>
 		</Wrapper>
 	);
 };
 
-const ListIcon = styled(FaListUl)`
-	background-color: lightgrey;
-	padding: 2px;
-	border: 1px solid black;
-	font-size: 20px;
-	border-radius: 4px;
-
-`;
-
 const Wrapper = styled.div`
+	max-width: 400px;
 	height: 500px;
 	max-height: 500px;
 	border: 1px solid black;
@@ -58,26 +62,65 @@ const Wrapper = styled.div`
 	padding: 24px;
 	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19),
 		0 8px 30px 0 rgba(0, 0, 0, 0.18);
+	transition: all ease 200ms;
+
+	&:hover {
+		cursor: pointer;
+		scale: 1.008;
+	}
 `;
 const ImageContainer = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	max-width: 100%;
-	max-height: calc((0.8 * 500px) - (2 * 24px));
+	height: calc((0.75 * 500px) - (2 * 24px));
 	overflow: hidden;
 `;
 
 const Image = styled.img`
 	display: block;
-	max-height: calc(0.8 * 500px);
-	object-fit: cover;
+	width: 100%;
 `;
 
-const TextContainer = styled.div`
+const RecipeName = styled.h1`
+	min-width: calc(300px - (2 * 24px));
+`;
+
+const TextAndIconContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	height: calc((0.28 * 500px) - (2 * 24px));
+	min-width: calc(300px - (2 * 24px));
+`;
+
+const LeftContainer = styled.div`
+	flex-grow: 2;
 	display: flex;
 	flex-direction: column;
-	align-items: start;
+	justify-content: space-between;
+`;
+
+const RightContainer = styled.div`
+	flex-grow: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	align-items: flex-end;
+`;
+
+const ListIcon = styled(FaListUl)`
+	padding: 2px;
+	border: 3px solid black;
+	border-radius: 4px;
+	font-size: 19px;
+	display: block;
+	opacity: ${(props) => (props.$isPresent ? "1" : "0.5")};
+`;
+
+const NoteIcon = styled(FaRegNoteSticky)`
+	font-size: 30px;
+	display: block;
+	opacity: ${(props) => (props.$isPresent ? "1" : "0.5")};
 `;
 
 export default RecipeCard;
--- myCookingManager/frontend/src/components/RecipeGrid.jsx
+++ myCookingManager/frontend/src/components/RecipeGrid.jsx
@@ -6,27 +6,67 @@ import RecipeCard from "./RecipeCard";
 const RecipeGrid = ({ recipes }) => {
 	const navigate = useNavigate();
 	return (
-		<Wrapper>
-			<div>
-				<button onClick={() => navigate("/catalogue")}>
-					Add a recipe
-				</button>
-			</div>
+		<FlexWrapper>
+			<Wrapper>
+				<ButtonContainer>
+					<AddRecipeButton onClick={() => navigate("/catalogue")}>
+						Add a recipe
+					</AddRecipeButton>
+				</ButtonContainer>
 
-			{recipes &&
-				recipes.map((recipe) => {
-					return <RecipeCard key={recipe.recipeId} recipe={recipe} />;
-				})}
-		</Wrapper>
+				{recipes &&
+					recipes.map((recipe) => {
+						return (
+							<RecipeCard key={recipe.recipeId} recipe={recipe} />
+						);
+					})}
+			</Wrapper>
+		</FlexWrapper>
 	);
 };
 
+const FlexWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
 const Wrapper = styled.div`
 	width: 90vw;
-	margin: 0 auto;
+	margin: 30px auto;
 	display: grid;
-	gap: 1rem;
-	grid-template-columns: repeat(auto-fit, minmax(300px, 400px));
+	align-items: center;
+	gap: 30px;
+	grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+`;
+
+const ButtonContainer = styled.div`
+	background-color: rgba(225, 217, 217, 0.588);
+	max-width: 400px;
+	height: 500px;
+	max-height: 500px;
+	border: 1px solid black;
+	border-radius: 10px;
+	padding: 24px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19),
+		0 8px 30px 0 rgba(0, 0, 0, 0.18);
+	transition: all ease 200ms;
+
+	// Alternative Box shadow
+	//0 4px 40px 0 rgba(0, 0, 0, 0.19), 0 6px 60px 0 rgba(0, 0, 0, 0.18), 0 8px 700px 0 rgba(0, 0, 0, 0.17);
+
+	&:hover {
+		cursor: pointer;
+		scale: 1.008;
+	}
+`;
+
+const AddRecipeButton = styled.button`
+	width: 100%;
+	height: 100%;
+	border: none;
+	background-color: transparent;
 `;
 
 export default RecipeGrid;
