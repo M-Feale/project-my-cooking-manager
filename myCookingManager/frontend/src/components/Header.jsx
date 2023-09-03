af87f8446304d70c37ba9e3e822584c7ea30a12e
M-Feale <marjolainefeale@gmail.com>
2023-09-03 18:54:27 -0400

    layout-completed-in-Header

--- myCookingManager/frontend/src/components/Header.jsx
+++ myCookingManager/frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { styled } from "styled-components";
-import LogoutButton from "./LogoutButton";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate } from "react-router";
+
+import LogoutButton from "./LogoutButton";
 
 const Header = () => {
+	// Import isAutheticated from auth0 to show navigation conditionally
 	const { isAuthenticated } = useAuth0();
 	const navigate = useNavigate();
 
@@ -12,7 +14,21 @@ const Header = () => {
 			<BrandHeading onClick={() => navigate("/")}>
 				My Cooking Manager
 			</BrandHeading>
-			{isAuthenticated && <LogoutButton />}
+			<NavAndLogoutContainer>
+				{isAuthenticated && (
+					<>
+						<NavBar>
+							<NavButtons to={"/recipes"}>
+								Recipe Collection
+							</NavButtons>
+							<NavButtons to={"/catalogue"}>
+								Recipe Cataloguing
+							</NavButtons>
+						</NavBar>
+						<LogoutButton />
+					</>
+				)}
+			</NavAndLogoutContainer>
 		</Wrapper>
 	);
 };
@@ -30,7 +46,37 @@ const BrandHeading = styled.h1`
 	color: var(--secondary-color);
 	font-size: var(--heading-font-size);
 	font-family: var(--heading-font-family);
-    cursor: pointer;
+	cursor: pointer;
+`;
+
+const NavAndLogoutContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	width: 50vw;
+`;
+
+const NavBar = styled.div`
+	width: 75%;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+`;
+
+const NavButtons = styled(NavLink)`
+	font-size: 16px;
+	margin: 0 10px;
+	font-family: var(--link-font-family);
+	text-decoration: none;
+	color: var(--secondary-color);
+
+	&.active {
+		color: var(--tertiary-color);
+		text-decoration: underline;
+		text-decoration-thickness: 2px;
+		text-underline-offset: 10px;
+	}
 `;
 
 // const Button = styled.button`
