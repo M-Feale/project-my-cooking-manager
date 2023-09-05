0a18ce805fcb65128f2aba315eac8649466fc84a
M-Feale <marjolainefeale@gmail.com>
2023-09-05 17:51:46 -0400

    modified-dialogue-button-colors

--- myCookingManager/frontend/src/components/DialogueButton.jsx
+++ myCookingManager/frontend/src/components/DialogueButton.jsx
@@ -15,15 +15,15 @@ const DialogueButton = ({ onClickFunc, text }) => {
 
 const Button = styled.button`
 	padding: 10px 20px;
-	background-color: var(--primary-color);
-	font-family: var(--link-font-family);
-	color: white;
 	margin: 0 10px;
-	border: 2px solid var(--primary-color);
 	min-width: 100px;
 	max-width: 175px;
 	border-radius: 3px;
-
+	font-family: var(--link-font-family);
+	border: 2px solid var(--tertiary-color);
+	background-color: var(--tertiary-color);
+	color: black;
+	
 	&:focus {
 		border: 2px solid black;
 		outline: none;
