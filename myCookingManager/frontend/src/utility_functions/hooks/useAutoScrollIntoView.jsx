5d6af1daaf061571574a016bf1b48bcb471dc712
M-Feale <marjolainefeale@gmail.com>
2023-09-05 17:55:10 -0400

    modified-useAutoScrolling-hook-to-work-with-options

--- myCookingManager/frontend/src/utility_functions/hooks/useAutoScrollIntoView.jsx
+++ myCookingManager/frontend/src/utility_functions/hooks/useAutoScrollIntoView.jsx
@@ -3,13 +3,15 @@ import { useEffect, useRef } from "react";
 const useAutoScrollIntoView = () => {
 	const ref = useRef(null);
 
+	const options = {
+		behavior: "smooth",
+		block: "start",
+		inline: "nearest",
+	};
+
 	useEffect(() => {
 		if (ref.current) {
-			ref.current.scrollIntoView({
-				behavior: "smooth",
-				block: "end",
-				inline: "end",
-			});
+			ref.current.scrollIntoView(options); 
 		}
 	}, []);
 	return ref;
