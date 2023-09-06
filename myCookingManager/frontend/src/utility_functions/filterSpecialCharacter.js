541621cf93248b7202466be8d4878ac57e352ac2
M-Feale <marjolainefeale@gmail.com>
2023-09-06 09:39:34 -0400

    edited-comments-for-clarity

--- myCookingManager/frontend/src/utility_functions/filterSpecialCharacter.js
+++ myCookingManager/frontend/src/utility_functions/filterSpecialCharacter.js
@@ -1,10 +1,11 @@
-export const filterSpecialCharacter = (url) => {
     // ------------------------------------------------------------------------------------------ //
     // Special characters break the link preview library (grabity) or return nothing.
     // Common special characters found in recipe urls are # and ?. Since grabity autocompletes 
-    // the rest of the url, I slice the url to keep everything before those 2 special characters.
+    // the rest of the url, this function was created to slice the url to keep everything
+    // before those 2 special characters.
     // ------------------------------------------------------------------------------------------ //
 
+export const filterSpecialCharacter = (url) => {
     const foundIndexOfHash = url.indexOf("#")
     if (foundIndexOfHash > 0) {
         const newUrl = url.slice(0, foundIndexOfHash)
