a70a70d227f8676506e389f694b404b010a925c6
M-Feale <marjolainefeale@gmail.com>
2023-09-06 09:37:13 -0400

    added-comments-to-contextualize-the-allRatingsCalculator-function

--- myCookingManager/frontend/src/utility_functions/allRatingsCalculator.js
+++ myCookingManager/frontend/src/utility_functions/allRatingsCalculator.js
@@ -1,3 +1,13 @@
+// ------------------------------------------------------------------------------------- //
+// This function handles a rating matching the provided label and makes an average
+// of the 3 ratings to set it as the "Overall" rating. It returns the "ratings" 
+// array with the right ratings. The "ratings" array contains 4 objects labeled
+//  "Overall", "Time Accuracy", "Easy Cleanup" and "Taste". 
+// The functions needs the value of one rating (a number used by the @smastrom/react-rating
+// library to handle the stars display), the label associated with that rating and
+// the complete ratings array.
+// ------------------------------------------------------------------------------------ //
+
 export const allRatingsCalculator = (rating, label, arrayOfRatingObjects) => {
 	const foundRating = arrayOfRatingObjects.find((rating) => {
 		return rating.label === label;
